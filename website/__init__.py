c0e0b455c39aa93ad240a67a4231ea1612fc1173
Ken Perez <rken.perez@example.com>
2025-09-01 07:13:01 +0800

    added create database

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,30 @@
 from flask import Flask
 from os import path
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "VerySecretKey"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    from .models import User
+
+    create_database(app) # Runs create_database
 
 
     from .views import views
     app.register_blueprint(views)
 
     return app
+
+# Creates Database
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print("Created Database!")
